73681a593b1b169c8133d20df2f47c427d0f6761
Rose Gold Minon <rgminon@Roses-MacBook.local>
2018-02-17 11:27:47 -0600

    initial commit

---
+++ spec/main_spec.rb
@@ -0,0 +1,9 @@
+describe "Application 'ruby_motion_testing'" do
+  before do
+    @app = UIApplication.sharedApplication
+  end
+
+  it "has one window" do
+    @app.windows.size.should == 1
+  end
+end
